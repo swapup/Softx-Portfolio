6b507a570dae173f1521ad86ecefcefd50d29045
xEo <swapnilvirkar02@gmail.com>
2021-01-07 11:11:31 +0530

    Project CSS js added Not complete

---
+++ ClickShowHide.js
@@ -0,0 +1,14 @@
+
+document.getElementById("image1").addEventListener("click",function(){
+    var Img = document.getElementById("Containt1");
+    if(Img.style.display=="none"){
+        Img.style.display="flex"
+    }
+    else{
+        Img.style.display="none"
+    }
+
+
+
+
+} )
